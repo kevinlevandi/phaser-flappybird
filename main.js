3c1cc10178092376b7872d8397aed2ca26535c5f
Kevin Levandi <kevin.levandi@gmail.com>
2017-07-30 22:44:05 +0700

    add pipe and collision

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var mainState = {
     preload: function() { 
-        game.load.image('bird', 'assets/bird.png'); 
+        game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() {
@@ -16,11 +17,20 @@ var mainState = {
 
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this); 
+
+        this.pipes = game.add.group();
+
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" }); 
     },
 
     update: function() {
         if (this.bird.y < 0 || this.bird.y > 490)
             this.restartGame();
+
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
@@ -29,6 +39,30 @@ var mainState = {
 
     restartGame: function() {
         game.state.start('main');
+    },
+
+    addOnePipe: function(x, y) {
+        var pipe = game.add.sprite(x, y, 'pipe');
+
+        this.pipes.add(pipe);
+
+        game.physics.arcade.enable(pipe);
+
+        pipe.body.velocity.x = -200; 
+
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+
+    addRowOfPipes: function() {
+        var hole = Math.floor(Math.random() * 5) + 1;
+
+        for (var i = 0; i < 8; i++)
+            if (i != hole && i != hole + 1) 
+                this.addOnePipe(400, i * 60 + 10);
+        
+        this.score += 1;
+        this.labelScore.text = this.score; 
     },
 };
 
